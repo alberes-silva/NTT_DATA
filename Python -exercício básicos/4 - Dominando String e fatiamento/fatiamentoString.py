c2ecad51afae200fbe1126ec1fbd64f31087413c
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-15 18:25:29 -0300

    Create fatiamentoString.py

---
+++ Python -exercício básicos/4 - Dominando String e fatiamento/fatiamentoString.py
@@ -0,0 +1,22 @@
+nome = 'Guilherme Arthur de Carvalho'
+
+#Pega o 1º indice, que começa por 0
+print(nome[0])
+
+#Pega o 8º indice
+print(nome[:9])
+
+#Pega a partir do 10 indice
+print(nome[10:])
+
+#Pega entre o 10 e 16 indice
+print(nome[10:16])
+
+#Pega entre o 10 e 16 indice em cada 2
+print(nome [10:16:2])
+
+#Retorna a string toda
+print(nome[:])
+
+#Inverte a string
+print(nome[::-1])
