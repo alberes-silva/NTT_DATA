ccf0a93f83aed855fab236353945374e0febb47b
Alberes Silva <alberessilva@yahoo.com.br>
2024-09-15 18:25:24 -0300

    Create while.py

---
+++ Python -exercício básicos/3 - Estrutura condicionais e de repetição/while.py
@@ -0,0 +1,14 @@
+#WHILE -> É usado para repetir um bloco de código várias vezes. Faz sentido usar while quando não sabemos o número exato de vezes
+#que nosso bloco de código deve ser executado
+
+opcao = 1
+
+while opcao != 0:
+    opcao = int(input('[1] Sacar \n[2] Extrato \n[0] Sair\n'))
+
+    if opcao == 1:
+        print('Sacado')
+    elif opcao == 2:
+        print('Exibindo extrato...')
+
+
